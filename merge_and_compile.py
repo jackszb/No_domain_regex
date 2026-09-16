71022f80562d21f9fa4686c7ee862d5784f159e7
jackszb <55304530+jackszb@users.noreply.github.com>
2026-09-16 19:05:12 +0800

    Update REJECT_URLS and fix main() call formatting
    Added a new URL to the REJECT_URLS list and ensured main() is called with a newline at the end of the file.

--- merge_and_compile.py
+++ merge_and_compile.py
@@ -21,6 +21,7 @@ PROXY_URLS = [
 
 REJECT_URLS = [
     "https://raw.githubusercontent.com/jackszb/hagezi/main/rules/pro.plus.json",
+    "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/Filters/AWAvenue-Ads-Rule-Singbox-Only.Ads.json",
     "https://raw.githubusercontent.com/jackszb/No_domain_regex/main/reject_custom_rules.json",
 ]
 
